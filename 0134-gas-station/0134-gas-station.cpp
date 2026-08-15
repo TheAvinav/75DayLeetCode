67c5e2e2bc42f53e1f7127733605abb792d02705
TheAvinav <avinav.mendu890@gmail.com>
2026-08-15 17:37:37 +0530

    Time: 7 ms (9.94%), Space: 112.4 MB (62.01%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) 
+    {
+        int answer = -1;
+
+        if (possible(gas, cost))
+        {
+            int tank = 0;
+            int start = 0;
+
+            for (int i = 0; i < gas.size(); i++)
+            {
+                tank = tank + gas[i] - cost[i];
+
+                if (tank < 0)
+                {
+                    tank = 0;
+                    start = i+1;
+                }
+            }
+            answer = start;
+        }
+        else
+        {
+            answer = -1;
+        }
+
+        return answer;
+    }
+
+    bool possible(vector<int>& gas, vector<int>& cost)
+    {
+        int tank = 0;
+
+        for (int i = 0; i < gas.size(); i++)
+        {
+            tank = tank + gas[i] - cost[i];
+        }
+
+        if (tank >= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
