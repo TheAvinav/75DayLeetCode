8a92087a5fdb89aaf9a1e00c3bca38ae1581fff5
TheAvinav <avinav.mendu890@gmail.com>
2026-08-29 13:11:46 +0530

    Time: 3 ms (25.75%), Space: 11 MB (27.23%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -8,16 +8,14 @@ public:
         }
 
         for(int j=i;j<=n;j++){
-            swap( nums[i],nums[j]);
+            swap(nums[i],nums[j]);
             permutation(nums,i+1,n);
-            swap( nums[i],nums[j]);
+            swap(nums[i],nums[j]);
         }
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
-        
-        permutation(nums,0,nums.size()-1);
-        
+        permutation(nums,0,nums.size()-1);        
         return result;
     }
 };
