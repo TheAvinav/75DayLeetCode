6e3fb0b320d8ca50200c3e60dba86f1afe1cd6ff
TheAvinav <avinav.mendu890@gmail.com>
2026-09-04 15:55:34 +0530

    Time: 491 ms (14.09%), Space: 8.8 MB (54.29%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,41 +1,50 @@
 class Solution {
 public:
     void solveSudoku(vector<vector<char>>& board) {
-        vector<vector<bool>> row(9, vector<bool>(9, false));
-        vector<vector<bool>> col(9, vector<bool>(9, false));
-        vector<vector<bool>> box(9, vector<bool>(9, false));
-        for(int i = 0; i < 9; i++)
-            for(int j = 0; j < 9; j++)
-                if(board[i][j] != '.'){
-                    int b = (i / 3) * 3 + (j / 3);
-                    row[i][board[i][j] - '0' - 1] = true;
-                    col[j][board[i][j] - '0' - 1] = true;
-                    box[b][board[i][j] - '0' - 1] = true;
-                }
-        solve(board, row, col, box);
+        solve(board);
     }
 
-    bool solve(vector<vector<char>>& board, vector<vector<bool>>& row, vector<vector<bool>>& col, vector<vector<bool>>& box){
-        int b;
-        for(int i = 0; i < 9; i++)
-            for(int j = 0; j < 9; j++)
-                if(board[i][j] == '.'){
-                    b = (i / 3) * 3 + (j / 3);
-                    for(int k = 0; k < 9; k++)
-                        if(!row[i][k] && !col[j][k] && !box[b][k]){
-                            row[i][k] = true;
-                            col[j][k] = true;
-                            box[b][k] = true;
-                            board[i][j] = '0' + k + 1;
-                            if(solve(board, row, col, box))
+    bool solve(vector<vector<char>>&board)
+    {
+        for (int i = 0; i < board.size(); i++)
+        {
+            for (int j = 0; j < board.size(); j++)
+            {
+                if (board[i][j] == '.')
+                {
+                    for (char k = '1'; k <= '9'; k++)
+                    {
+                        if (isValid(board, i, j, k))
+                        {
+                            board[i][j] = k;
+                            if (solve(board) == true)
+                            {
                                 return true;
-                            board[i][j] = '.';
-                            row[i][k] = false;
-                            col[j][k] = false;
-                            box[b][k] = false;
+                            }
+                            else
+                            {
+                                board[i][j] = '.';
+                            }
                         }
+                    }
+
                     return false;
                 }
+            }
+        }
+        return true;
+    }
+
+    bool isValid(vector<vector<char>> &board, int row, int col, char k)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (board[i][col] == k) return false;
+            if (board[row][i] == k) return false;
+
+            if (board[ 3 * (row/3) + i / 3][ 3 * (col/3) + i % 3] == k) return false;
+        }
+
         return true;
     }
 };
