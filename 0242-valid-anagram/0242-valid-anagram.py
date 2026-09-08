970a8d05b1fc289b2c7cd97908e0eede8ee2af4e
TheAvinav <avinav.mendu890@gmail.com>
2026-09-08 22:29:12 +0530

    Time: 4 ms (98.64%), Space: 12.5 MB (86.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,12 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        
+        for idx in set(s):
+            if s.count(idx) != t.count(idx):
+                return False
+        return True 
+
+
         
