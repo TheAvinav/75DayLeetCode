e139fd47e9d8cd87afb51e0105b9bcd7c435f272
TheAvinav <avinav.mendu890@gmail.com>
2026-09-20 15:17:35 +0530

    Time: 3 ms (16.14%), Space: 9.6 MB (95.77%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,9 +1,14 @@
 class Solution {
-public:
-    int reverseDegree(string& s) {
-        int sum=0, n=s.size();
-        for(int i=0; i<n; i++) {
-            sum += (i+1)*('z'-s[i]+1);
+    public:
+    int reverseDegree(string s) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s[i];
+
+            int reverseValue = 26 - (c - 'a');
+            int position = i + 1;
+
+            sum += reverseValue * position;
         }
         return sum;
     }
